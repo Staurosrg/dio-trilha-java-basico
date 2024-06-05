8bf95b970395055f12ec2aabadca8b22ad2fe4d1
Rogerio Silva <rogerio.trovata@gmail.com>
2024-06-05 10:06:40 -0300

    criação da mensagem

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nomeCliente = "Rogerio Silva";
+        int numero = 1021;
+        double saldo = 237.48;
+        String agencia = "067-8";
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.print("Por favor digite o número da agencia sem o digito: ");
+        String numeroAgencia = scanner.next();
+          
+        System.out.print("Por favor digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+            System.out.println("Olá "+ nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta " + numero + " e seu saldo "+saldo+" já esta disponivel para saque");
+
     }
 }
+
+

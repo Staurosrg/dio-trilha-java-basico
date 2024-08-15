8ac196c49bda51de21062420acea63fed9caf5f4
Rogerio Silva <rogerio.trovata@gmail.com>
2024-08-15 14:39:57 -0300

    correção no metodo de scanner

--- src/Contador.java
+++ src/Contador.java
@@ -1,24 +1,35 @@
+
+import java.util.Scanner;
+
 public class Contador {
-	public static void main(String[] args) {
-		Scanner terminal = new Scanner(System.in);
-		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.??;
-		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.??;
-		
-		try {
-			//chamando o método contendo a lógica de contagem
-			contar(parametroUm, parametroDois);
-		
-		}catch (? exception) {
-			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
-		}
-		
-	}
-	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
-		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-		
-		int contagem = parametroDois - parametroUm;
-		//realizar o for para imprimir os números com base na variável contagem
-	}
+public static void main(String[] args) {
+    int parametroUm;
+    int parametroDois;
+    try (Scanner dadosEntrada = new Scanner(System.in)) {
+        System.out.println("Digite o primeiro parâmetro");
+        parametroUm = dadosEntrada.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        parametroDois = dadosEntrada.nextInt();
+    }
+
+    try {
+        contar(parametroUm, parametroDois);
+    } catch (ParametrosInvalidosException exception) {
+        System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+    }
+}
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+class ParametrosInvalidosException extends Exception {
+   
 }
